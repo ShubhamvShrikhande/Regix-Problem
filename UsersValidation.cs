872fe286cc9107b714284719734a2e3178cc1249
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-02-14 13:34:57 +0530

    UC2-Added

--- UsersValidation.cs
+++ UsersValidation.cs
@@ -9,11 +9,20 @@ namespace Regix_problem
 {
     internal class UsersValidation
     {
-        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
         public void ValidationFirstName(string firstname)
         {
             Regex regex = new Regex(FIRST_NAME_REGEX);
             bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+        }
+        //UC2
+        //LastName the First Letter Should Capital and minimum 3 Characters
+        public string LASTNAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationLastName(string lastname)
+        {
+            Regex regex = new Regex(LASTNAME_REGEX);
+            bool result = regex.IsMatch(lastname);
             Console.WriteLine(result);
         }
     }
