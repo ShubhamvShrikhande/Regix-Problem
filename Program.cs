26dd5a78cccd67fcb5e0506b6efdb750d6fbea1e
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-02-14 12:52:26 +0530

    UC1-Added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Regix_problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
+            UsersValidation userValidation = new UsersValidation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstname = Console.ReadLine();
+            userValidation.ValidationFirstName(firstname);
+        }
+    }
+}
